5482feffa7635a38a950e784c0da1f974c8364d0
Ondapufo <ondapufo@posteo.net>
2019-04-09 10:51:44 -0700

    Updated todo list.

--- kenobi-simulator.c
+++ kenobi-simulator.c
@@ -23,6 +23,9 @@
 
 //Purpose: To screw around with what I learned from intro to C.
 
+//Neswest idea: Rewrite using ncurses.
+//				Rewrite the chet characters part using what I know now.
+
 // NEW IDEAS:
 //		The data structure is almost ok. Could just add a kinobi quote and the
 //			order in which they are supposed to appear.
